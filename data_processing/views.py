8d4d3b4a2325248ffc4f8f68f4c2e521e98181f7
ShaneNick <shanetomonia@gmail.com>
2024-03-05 21:24:16 -0500

    able to filter out the games won by comparing the shots sheet to the points sheet to see who won

--- data_processing/views.py
+++ data_processing/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import OutcomeSerializer
+from .data.match_data import get_match_outcomes  
+
+
+
+def outcomes_page(request):
+    # Fetch the outcomes data using function
+    outcomes_data = get_match_outcomes().to_dict('records')  # Convert DataFrame to a list of dicts
+    
+    # Prepare the context with the outcomes data
+    context = {'outcomes': outcomes_data}
+    
+    # Render the 'outcomes.html' template with the outcomes data
+    return render(request, 'data_processing/outcomes.html', context)
